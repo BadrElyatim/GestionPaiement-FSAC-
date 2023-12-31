1863ff5928a813a986434aa7b678677b535a17b3
badr elyatim <flipbadr7@gmail.com>
2023-12-31 11:39:37 +0100

    installing & configuring breeze

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class () extends Migration {
             $table->string('email')->unique();
             $table->string('telephone')->unique();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->enum('role', ['admin', 'professeur', 'regisseur']);
             $table->rememberToken();
             $table->timestamps();
