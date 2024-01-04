394eac9aad4e86e100f195a226fee5d7b737e9d6
badr elyatim <flipbadr7@gmail.com>
2024-01-04 18:10:35 +0100

    adding fake db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +15,21 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::create([
+            'nom' => 'admin',
+            'prenom' => 'admin',
+            'telephone' => '0613431414',
+            'password' => Hash::make('admin'),
+            'role' => 'admin',
+            'email' => 'admin@gmail.com',
+        ]);
+        \App\Models\User::create([
+            'nom' => 'reg',
+            'prenom' => 'reg',
+            'telephone' => '05436546456',
+            'password' => Hash::make('reg'),
+            'role' => 'regisseur',
+            'email' => 'reg@gmail.com',
+        ]);
     }
 }
