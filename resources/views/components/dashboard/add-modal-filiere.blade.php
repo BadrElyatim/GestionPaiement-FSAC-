3786d7a2fa94c69a28285f8de008e5a554f5b596
badr elyatim <flipbadr7@gmail.com>
2024-01-05 09:51:11 +0100

    changing to type string of column annee_universitaire of filieres table

--- resources/views/components/dashboard/add-modal-filiere.blade.php
+++ resources/views/components/dashboard/add-modal-filiere.blade.php
@@ -45,7 +45,7 @@
                     </div>
                     <div class="col-span-2 sm:col-span-1">
                         <label for="annee_universitaire" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Annee Universitaire</label>
-                        <input type="date" name="annee_universitaire" value="{{ explode(' ', now())[0] }}" id="annee_universitaire" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required="">
+                        <input type="text" name="annee_universitaire" value="{{ now()->month >= 9 ? now()->year.'/'.now()->addYear()->year : now()->subYear()->year.'/'.now()->year }}" id="annee_universitaire" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required="">
                     </div>
                     <div class="col-span-2 sm:col-span-1">
                         <label for="professeur" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">professeur</label>
